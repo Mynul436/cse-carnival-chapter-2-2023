5ddcebcd2c88a2933c472d0965686217bd3a6e35
Mynul436 <mynulakash436@gmail.com>
2023-10-14 18:44:21 +0600

    logout setting

--- Tech-Titans/resources/views/components/backend/section-header.blade.php
+++ Tech-Titans/resources/views/components/backend/section-header.blade.php
@@ -23,9 +23,34 @@
         @endif
     </div>
     @if($toolbar)
-    <div class="btn-toolbar d-block text-end" role="toolbar" aria-label="Toolbar with buttons">
+    {{-- <div class="btn-toolbar d-block text-end" role="toolbar" aria-label="Toolbar with buttons">
         {{ $toolbar }}
+    </div> --}}
+    {{-- <h1>asdfads</h1> --}}
+
+    @auth
+    <div x-show="isUserMenuOpen" @click.away="isUserMenuOpen = false" x-transition:enter="transition ease-out duration-100 transform" x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75 transform" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-2 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+
+        {{-- @can('view_backend')
+        <a href='{{ route("backend.dashboard") }}' class="block px-4 py-2 text-sm text-gray-600 hover:bg-orange-600 hover:text-white" role="menuitem">
+            <i class="fas fa-tachometer-alt fa-fw"></i>&nbsp;{{__('Dashboard')}}
+        </a>
+        @endif
+        <a href="{{ route('frontend.users.profile', encode_id(auth()->user()->id)) }}" class="block px-4 py-2 text-sm text-gray-600 hover:bg-orange-600 hover:text-white" role="menuitem">
+            <i class="fas fa-user fa-fw"></i>&nbsp;{{ Auth::user()->name }}
+        </a> --}}
+        <a href="{{ route('frontend.users.profileEdit', encode_id(auth()->user()->id)) }}" class="block px-4 py-2 text-sm text-gray-600 hover:bg-orange-600 hover:text-white" role="menuitem">
+            <i class="fas fa-cogs fa-fw"></i>&nbsp;{{__('Settings')}}
+        </a>
+        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="block px-4 py-2 text-sm text-gray-600 hover:bg-orange-600 hover:text-white" role="menuitem">
+            {{__('Logout')}}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+        </form>
     </div>
+    @endauth
     @else
     <div class="btn-toolbar d-block text-end" role="toolbar" aria-label="Toolbar with buttons">
         @if (Str::endsWith(Route::currentRouteName(), 'create'))
